f4f0b3076299b0580d776cd3c84610969d89054d
sushanth91 <123243296+sushanth91@users.noreply.github.com>
2023-06-07 16:32:02 +0530

    Update UserRepository.java
    Custom delete Query added in UserRepository

--- exceptionhanding-springboot-validation/src/main/java/com/exception/repository/UserRepository.java
+++ exceptionhanding-springboot-validation/src/main/java/com/exception/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.exception.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Modifying;
+// import org.springframework.data.jpa.repository.Query;
 
 import com.exception.entity.User;
 
 public interface UserRepository  extends JpaRepository<User,Integer> {
     User findByUserId(int id);
+//     @Modifying
+//     @Transactional
+//     @Query("Delete From Student where firstName = :firstName")
+//     Integer deleteByFirstName (String firstName);
 }
